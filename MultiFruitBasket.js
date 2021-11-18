9b283a026a3029cf0ca4affb2cc42fe9cec462c2
mafoloEmmanuel <mafoloemmanuel1@gmail.com>
2021-11-19 01:45:03 +0200

    done

--- MultiFruitBasket.js
+++ MultiFruitBasket.js
@@ -1,27 +1,79 @@
 module.exports = (pool) => {
 
-    //create a fruit basket for a given fruit type,qty and price
+    // get multi fruit basket Id
+    let getMultiBasketsId = async (basketName) => {
+        var result = await pool.query('select id from multi_fruit_basket where name=$1 ', [basketName]);
+        return result.rows[0].id
+    }
+    // *** create a fruit basket for a given fruit type,qty and price
 
-    let createBasket = async (type, price, qty) => {
-        var result = await pool.query('insert into fruit_basket_item (fruit_name,price,quantity) values ($1,$2,$3)',[type,price,qty])
+    let setFruitBasketItem = async (type, price, qty, basketName) => {
+        var setId = await getMultiBasketsId(basketName);
+        var result = await pool.query('insert into fruit_basket_item (fruit_name,price,quantity,multi_id) values ($1,$2,$3,$4)', [type, price, qty, setId])
         return result.rows
     }
-
-    let getAllBaskets=async()=>{
+    // *** Add to existing basket
+    let setMultiFruitBasket = async (id, basketName) => {
+        var result = await pool.query('insert into multi_fruit_basket (id,name) values ($1,$2)', [id, basketName])
+        return result.rows
+    }
+    // *** Get all the baskets
+    let getAllBaskets = async () => {
         var result = await pool.query('select fruit_name,price,quantity from fruit_basket_item ')
         return result.rows
     }
 
-    //add fruits to an existing basket
-    let addToExistingBasket=async(name)=>{
-        var result = await pool.query('insert into multi_fruit_basket(name) values($1)',[name] );
+    let getMultiBaskets = async () => {
+        var result = await pool.query('select name from multi_fruit_basket')
+       console.log('Lovely')
         return result.rows
     }
-    
 
-    return{
-        createBasket,
+
+    /*remove fruits from an existing basket - 
+    if there are no fruit left in the basket the basket should be removed*/
+
+    let removeFromBasket = async (type, id) => {
+        var removeItem = await pool.query('delete from fruit_basket_item where fruit_name=$1', [type])
+        var checkExistingBaskets = await pool.query('select fruit_name,price,quantity,name from fruit_basket_item, multi_fruit_basket where fruit_basket_item.multi_id=multi_fruit_basket.id');
+        if (checkExistingBaskets.rows.length < 0) {
+            var removeAgain = await pool.query('delete from multi_fruit_basket where id=$1', [id]);
+            return removeAgain.rows;
+        }
+        return removeItem.rows
+    }
+
+    // *** for a given id return the basket_name & id as well as a list of all the fruits in the basket
+let getAllFromBothTables =async()=>{
+    var result= await pool.query('select f.fruit_name,f.price,f.quantity,m.id,m.name from fruit_basket_item f , multi_fruit_basket m where f.multi_id=m.id')
+
+return result.rows
+}
+
+/* return the total cost of a specific basket based: 
+----> basket name
+----> the basket id 
+*/
+let getAllBasketTotals = async()=>{
+    var result = await pool.query(' select m.id,m.name ,sum(f.price*f.quantity) from multi_fruit_basket m join fruit_basket_item f on m.id=f.multi_id group by m.id');
+    return result.rows
+}
+
+let getSpecificBasketTotal =async(type)=>{
+    var result = await pool.query(' select m.id,m.name ,sum(f.price*f.quantity) from multi_fruit_basket m join fruit_basket_item f on m.id=f.multi_id where f.fruit_name=$1 group by m.id',[type]);
+    return result.rows
+
+}
+    return {
+        getMultiBasketsId,
+        setFruitBasketItem,
+        setMultiFruitBasket,
         getAllBaskets,
-        addToExistingBasket
+        getMultiBaskets,
+        removeFromBasket,
+        getAllFromBothTables,
+        getAllBasketTotals,
+        getSpecificBasketTotal
     }
 }
+
--- test/test.js
+++ test/test.js
@@ -1,59 +1,181 @@
 const { Pool } = require('pg');
-const assert = require('assert')
+const assert = require('assert');
 const MultiFruitBasket = require('../MultiFruitBasket')
 
 let useSSL = false;
 let local = process.env.LOCAL || false;
 if (process.env.DATABASE_URL && !local) {
-    useSSL = { rejectUnauthorized: false };
+  useSSL = { rejectUnauthorized: false };
 }
 const connectionString = process.env.DATABASE_URL || 'postgresql://coder:201735469@localhost:5432/coderdb'
 const pool = new Pool({
-    connectionString: connectionString,
-    ssl: useSSL
+  connectionString: connectionString,
+  ssl: useSSL
 });
 pool.on('connect', () => {
   console.log('connection has started');
 });
-describe('Multi fruit basket', ()=>{
-    beforeEach(async()=>{
-await pool.query('delete from fruit_basket_item')
-    })
-    const multiFruity = MultiFruitBasket(pool);
-it('set baskets', async()=>{
-await multiFruity.createBasket('Apple',3.00,5);
-await multiFruity.createBasket('Banana',1.00,5);
-await multiFruity.createBasket('Mango',2.00,5);
-await multiFruity.createBasket('Orange',2.00,5);
+describe('Multi fruit basket', () => {
+  let multiFruity = MultiFruitBasket(pool)
+  beforeEach(async () => {
+    await pool.query('delete from fruit_basket_item');
+    await pool.query('delete from multi_fruit_basket');
 
-assert.deepEqual(
-    [
+    //insert values into the tables
+    await pool.query("insert into multi_fruit_basket (id,name) values (25,'Red Apples') ");
+   await pool.query("insert into fruit_basket_item (fruit_name,price,quantity,multi_id)  values('Apples', 2.50, 15, 25)");
+   
+   await pool.query("insert into multi_fruit_basket (id,name) values (41,'Yellow Bananas') ");
+   await pool.query("insert into fruit_basket_item (fruit_name,price,quantity,multi_id)  values('Bananas', 1.00, 20, 41)");
+  
+   await pool.query("insert into multi_fruit_basket (id,name) values (62,'Orange Oranges') ");
+   await pool.query("insert into fruit_basket_item (fruit_name,price,quantity,multi_id)  values('Oranges', 2.00, 30, 62)");
+  
+   await pool.query("insert into multi_fruit_basket (id,name) values (45,'Green Pears') ");
+   await pool.query("insert into fruit_basket_item (fruit_name,price,quantity,multi_id)  values('Pears', 4.00, 38, 45)");
+
+  });
+  it('Should get all the fruit baskets', async()=>{
+      
+    assert.deepEqual(   [
+        {
+          fruit_name: 'Apples',
+          price: '2.50',
+          quantity: 15
+        },
+        {
+          fruit_name: 'Bananas',
+          price: '1.00',
+          quantity: 20
+        },
+        {
+          fruit_name: 'Oranges',
+          price: '2.00',
+          quantity: 30
+        },
+        {
+          fruit_name: 'Pears',
+          price: '4.00',
+          quantity: 38
+        }
+      ], await multiFruity.getAllBaskets())
+  });
+  it('Show all the basket names in the multi fruit basket', async()=>{
+    assert.deepEqual([
       {
-        fruit_name: 'Apple',
-        price: '3.00',
-        quantity: 5
+        name: 'Red Apples'
       },
       {
-        fruit_name: 'Banana',
+        name: 'Yellow Bananas'
+      },
+      {
+        name: 'Orange Oranges'
+      },
+      {
+        name: 'Green Pears'
+      },
+      
+    ], await multiFruity.getMultiBaskets());
+  
+  });
+  it('Remove fruit basket item from the basket and later remove the whole basket details', async()=>{
+    await multiFruity.removeFromBasket('Pears',45);
+
+    assert.deepEqual([
+      {
+        fruit_name: 'Apples',
+        price: '2.50',
+        quantity: 15
+      },
+      {
+        fruit_name: 'Bananas',
         price: '1.00',
-        quantity: 5
+        quantity: 20
       },
       {
-        fruit_name: 'Mango',
+        fruit_name: 'Oranges',
         price: '2.00',
-        quantity: 5
-      },
-      {
-        fruit_name: 'Orange',
-        price: '2.00',
-        quantity: 5
+        quantity: 30
       }
-    ],await multiFruity.getAllBaskets())
+    ]
+    , await multiFruity.getAllBaskets());
+  });
 
-});
+  it('for a given id return the basket_name & id as well as a list of all the fruits in the basket', async()=>{
+    assert.deepEqual(
+      [
+        {
+          fruit_name: 'Apples',
+          id: 25,
+          name: 'Red Apples',
+          price: '2.50',
+          quantity: 15
+        },
+        {
+          fruit_name: 'Bananas',
+          id: 41,
+          name: 'Yellow Bananas',
+          price: '1.00',
+          quantity: 20
+        },
+        {
+          fruit_name: 'Oranges',
+          id: 62,
+          name: 'Orange Oranges',
+          price: '2.00',
+          quantity: 30
+        },
+        {
+          fruit_name: 'Pears',
+          id: 45,
+          name: 'Green Pears',
+          price: '4.00',
+          quantity: 38
+        }
+      ], await multiFruity.getAllFromBothTables())
+  });
+  it('Show the total cost for a specific basket',async()=>{
+    
+    assert.deepEqual([
+      {
+        id: 25,
+        name: 'Red Apples',
+        sum: '37.50'
+      }
+    ], await multiFruity.getSpecificBasketTotal('Apples') )
+  });
+  it('Show the total cost for all the fruit baskets',async()=>{
+    
+    assert.deepEqual(
+      [
+        {
+          id: 41,
+          name: 'Yellow Bananas',
+          sum: '20.00'
+        },
+        {
+          id: 62,
+          name: 'Orange Oranges',
+          sum: '60.00'
+        },
+        {
+          id: 25,
+          name: 'Red Apples',
+          sum: '37.50'
+        },
+        {
+          id: 45,
+          name: 'Green Pears',
+          sum: '152.00'
+        }
+      ], await multiFruity.getAllBasketTotals('Apples') )
+  });
 
-after(()=>{
-    pool.end();
-})
-})
+    after(() => {
+      pool.end();
+    })
+  });
+
+
+
 
